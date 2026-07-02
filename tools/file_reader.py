92fd85587ee5dfea7e8f7307658167c6342cc27d
ItzChaBoiGhoza <cguy925@gmail.com>
2026-07-02 00:32:20 -0600

    file reader tools

---
+++ tools/file_reader.py
@@ -0,0 +1,30 @@
+from langchain.tools import tool
+from pathlib import Path
+from docx import Document
+
+@tool("file_reader")
+def read_file(file_path:str) -> str:
+    """Reads a file and returns its contents as plain text.
+    
+    Accepts .txt and .docx file types.
+    
+    Args:
+        file_path: The full path to the file to read.
+    Returns:
+        The contents of the file as a plain string.
+    """
+    path = Path(file_path)
+    if path.exists():
+        if path.suffix.lower() == '.txt':
+            with open(file_path) as f:
+                return(f.read())
+        elif path.suffix.lower() == '.docx':
+            doc = Document(file_path)
+            paragraphs = []
+            for paragraph in doc.paragraphs:
+                paragraphs.append(paragraph.text)
+            return "\n".join(paragraphs)
+    else:
+        return("No file exists")
+    
+    return 
