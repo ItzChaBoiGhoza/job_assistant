336e30c4bf434bb2d7a618a4d4bf8345508340cf
ItzChaBoiGhoza <cguy925@gmail.com>
2026-07-11 00:57:54 -0600

    final production for version 1 of the app

--- main.py
+++ main.py
@@ -1,17 +1,62 @@
 from dotenv import load_dotenv
 from pathlib import Path
 import os
+import re
+import json
 from agent import create_job_agent
+from tools.file_tools import read_file, check_file, file_exporter
+from tools.analysis_tools import extract_keywords, score_resumes
+from tools.writing_tools import resume_tailor_tool, cover_letter_generator, interview_prep_generation
 
 load_dotenv()
 api_key = os.getenv('ANTHROPIC_API_KEY')
 output_dir = os.getenv('OUTPUT_DIR', "~/job_applications")
-path = Path(output_dir).expanduser().resolve()
+output_path = Path(output_dir).expanduser().resolve()
 
 def main():
     
-    create_job_agent(api_key)
-    print("Agent was created successfully!")
+    # create_job_agent(api_key)
+    # print("Agent was created successfully!")
     
+    job_description = read_file.invoke('inputs/job_description.txt')
+    extracted_keywords = extract_keywords.invoke(job_description)
+    extracted_keywords = re.sub(r'```json|```', '', keywords).strip()
+    resume_scores = score_resumes.invoke({"keywords": keywords, "resume_path": "inputs/resumes"})
+    resume_scores = re.sub(r'```json|```', '', scores).strip()
+    
+    keywords_data = json.loads(extracted_keywords)
+    company_name = keywords_data['company_name']
+    job_title = keywords_data['job_title']
+    
+    scores_data = json.loads(resume_scores)
+    winning_resume = scores_data['winning_resume']
+    suggestions = scores_data['suggestions']
+    
+    winning_resume_content = check_file(f'inputs/resumes/{winning_resume}')
+    
+    feedback = ""
+    
+    while True:
+        tailored_resume = resume_tailor_tool.invoke({"winning_resume": winning_resume_content,  "suggestion": "\n".join(suggestions), "keywords": extracted_keywords, "feedback": feedback})
+        
+        print("\n--- TAILORED RESUME PREVIEW ---\n")
+        print(tailored_resume)
+        print("\n--- END OF PREVIEW ---\n")
+        
+        approval = input('Do you want to export this resume? (yes/no): ')
+        
+        if approval.lower() == 'yes':
+            break
+        else:
+            feedback = input('What would like to change?')
+    
+    tailored_cover_letter = cover_letter_generator.invoke({"keywords": extracted_keywords, "tailored_resume": tailored_resume, "company_name": company_name, "job_title": job_title})
+    
+    interview_prep = interview_prep_generation.invoke(extracted_keywords)
+    
+    file_exporter.invoke({"tailored_resume": tailored_resume, "tailored_cover_letter": tailored_cover_letter, "job_description": job_description, "company_name": company_name, "job_title": job_title, "interview_prep": interview_prep})
+    
+    print(f"Application materials saved to: {output_path/company_name/job_title}")
+        
 if __name__ == '__main__':
     main()
